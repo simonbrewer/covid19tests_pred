8a05ac5c9270405f9ccd576831b275c47877a15e
Simon Brewer <brewersi@gmail.com>
2020-09-29 21:23:38 -0600

    Added more PDP plots

--- COVID19_ts_ranger_dalex.R
+++ COVID19_ts_ranger_dalex.R
@@ -84,6 +84,10 @@ ve_p <- variable_profile(explain_rf, variables = "daysSinceC", type = "partial")
 ve_p$color = "_label_"
 plot(ve_p)
 
+ve_p <- variable_profile(explain_rf, variables = "pnursing", type = "partial")
+ve_p$color = "_label_"
+plot(ve_p)
+
 ve_p <- variable_profile(explain_rf, variables = "lIncome", type = "partial")
 ve_p$color = "_label_"
 plot(ve_p)
@@ -106,6 +110,24 @@ wy$wday <- wday(ddate, week_start = 1, label = TRUE)
 bd <- variable_attribution(explain_rf, wy[1,], type = "break_down")
 plot(bd)
 pdf("wy_1.pdf")
+plot(bd)
+dev.off()
+
+sc <- newdat %>% 
+  filter(state == "SC") %>% 
+  select(date, lpState_popn, lpPop_o_60, lpPop_m, lpPop_white, 
+         lpPop_black, lpPop_AmIndAlNat, lpPop_asia, lpPop_NaHaPaIs,
+         lIncome, lpBachelor, phospitals, pnursing, puniversities,
+         pcaseNew_lag, daysSinceC, pdeathNew_lag, daysSinceD, hospRate, wday) %>%
+  group_by(date) %>%
+  summarise(across(lpState_popn:hospRate, mean, na.rm= TRUE))
+
+ddate <- ymd(sc$date)
+sc$wday <- wday(ddate, week_start = 1, label = TRUE)
+
+bd <- variable_attribution(explain_rf, sc[1,], type = "break_down")
+plot(bd)
+pdf("sc_1.pdf")
 plot(bd)
 dev.off()
 
