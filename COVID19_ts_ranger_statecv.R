df867fbb6e93d425a370f2121b24f6d3cd2728bc
Alexander Hohl <34194710+alexandster@users.noreply.github.com>
2020-12-08 16:00:06 -0700

    Add files via upload

---
+++ COVID19_ts_ranger_statecv.R
@@ -0,0 +1,130 @@
+## ----setup, include=FALSE-------------------------------------------------------------------
+knitr::opts_chunk$set(echo = TRUE)
+
+
+## ----message = FALSE------------------------------------------------------------------------
+set.seed(1234)
+library(dplyr)
+library(skimr)
+library(caret)
+library(gbm) ## For importance scores
+library(ggpubr)
+library(vip)
+library(pdp)
+
+
+my_log <- file("./outputs/COVDI19_ts_ranger_statecv.txt") # File name of output log
+sink(my_log, append = TRUE, type = "output") # Writing console output to log file
+sink(my_log, append = TRUE, type = "message")
+
+load("./outputs/covid19.RData")
+
+dat <- dat %>%
+  filter(!is.na(pcaseNew_lag))
+
+dat$ltest_rate <- log(dat$test_rate+1e-5)
+
+# dat <- dat %>%
+#   filter(state %in% c("CT", "MI", "NY", "WA"))
+
+## -------------------------------------------------------------------------------------------
+f1 <- ltest_rate ~ lpState_popn + lpPop_o_60 + lpPop_m + lpPop_white + 
+  lpPop_black + lpPop_AmIndAlNat + lpPop_asia + lpPop_NaHaPaIs +
+  lIncome + lpBachelor + phospitals + pnursing + puniversities +
+  pcaseNew_lag + daysSinceC + pdeathNew_lag + daysSinceD + hospRate + wday # + sTest
+
+## -------------------------------------------------------------------------------------------
+states = unique(sort(dat$state))
+nstates = length(states)
+
+
+## -------------------------------------------------------------------------------------------
+mod_results <- data.frame(states,
+                          RMSE = rep(NA, nstates),
+                          Rsquared = rep(NA, nstates),
+                          MAE = rep(NA, nstates)
+)
+
+
+## -------------------------------------------------------------------------------------------
+base_results <- data.frame(states,
+                          RMSE = rep(NA, nstates),
+                          Rsquared = rep(NA, nstates),
+                          MAE = rep(NA, nstates)
+)
+
+
+## -------------------------------------------------------------------------------------------
+## Update this if the hyperparameters change
+parGrid = expand.grid(mtry = 6, splitrule = "variance", min.node.size = 4)
+
+## -------------------------------------------------------------------------------------------
+for (i in 1:nstates) {
+  print(states[i])
+  state_id = which(dat$state == states[i])
+  training = dat[-state_id,]
+  testing =  dat[ state_id,]
+  
+  ltr_hist <- hist(training$ltest_rate, plot = FALSE,
+                   seq(min(training$ltest_rate), max(training$ltest_rate), length.out = 100))
+  wgt_vec <- 1 / ltr_hist$counts
+  casewgt <- wgt_vec[cut(training$ltest_rate, include.lowest = TRUE,
+                         breaks = ltr_hist$breaks, labels = FALSE)]
+  
+  ## Get baseline
+  # testing$baseline = testing$pState_popn
+  testing$baseline <- ((testing$sTest * testing$pState_popn) / testing$Tot_pop) * 1e3
+  
+  modFit <- train(
+    f1,
+    data = training,
+    method = "ranger",
+    ## Center and scale the predictors for the training
+    ## set and all future samples.
+    preProc = c("center", "scale"),
+    ## increase parameter set
+    tuneGrid = parGrid,
+    ## added:
+    num.trees = 250,
+    weights = casewgt,
+    # importance = 'permutation',
+    trControl = trainControl(method = "cv")
+    )
+  
+  ## Plot tuning results
+  # ggplot(modFit)
+  pred_test <- predict(modFit, newdata = testing)
+  results <- postResample(pred = exp(pred_test), obs = testing$test_rate)
+  # results <- postResample(pred = exp(pred_test), obs = testing$test_rate)
+  mod_results$RMSE[i] <- results[1]
+  mod_results$Rsquared[i] <- results[2]
+  mod_results$MAE[i] <- results[3]
+  
+  results <- postResample(pred = testing$baseline, obs = testing$test_rate)
+  base_results$RMSE[i] <- results[1]
+  base_results$Rsquared[i] <- results[2]
+  base_results$MAE[i] <- results[3]
+  
+  mydf = data.frame(obs = rep(testing$test_rate, 2), 
+                    pred = c(testing$baseline, exp(pred_test)),
+                    type = rep(c("base", "pred"), each = length(testing$baseline)))
+  p1 = ggscatter(mydf, x = "obs", y = "pred", col = "type", 
+            main = paste0("COVID 19 testing (",states[i],", raw-scale)")) + 
+    geom_abline()
+  ggsave(file = paste0("./figures/cv/covid_",states[i],"_ranger.png"), 
+         p1, device = "png")
+}
+
+
+## -------------------------------------------------------------------------------------------
+print("model results")
+knitr::kable(mod_results, digits = 4)
+print("basemodel results")
+knitr::kable(base_results, digits = 4)
+
+print("mean(base_results$MAE), mean(mod_results$MAE)")
+print(paste(mean(base_results$MAE), mean(mod_results$MAE)))
+
+save(mod_results, base_results, file = "./outputs/cvResults.RData")
+
+closeAllConnections() # Close connection to log file
